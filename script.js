3152d915de6d132330f9bd70e1321682b54f59b0
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2022-09-18 23:02:45 -0700

    Finished HTML and CSS. Configuring JS

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 // module pattern uses private and public methods/properties, return keyword, and IIFE
 // Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module.
 // If you need multiples of something (players!), create them with factories.
+// X is 1 or true,
+// O is -1 or false
+"use strict";
 
-const gameBoard = (function (checkData) {
+const gameBoard = (function () {
   let gameBoardData = [
     [0, 0, 0],
     [0, 0, 0],
@@ -18,8 +21,6 @@ const gameBoard = (function (checkData) {
       "click",
       () => {
         placeData(index);
-        console.log("check winning");
-        // FIXME: Pass in checkData in here
         checkData();
       },
       { once: true }
@@ -32,32 +33,72 @@ const gameBoard = (function (checkData) {
     let row = (index - col) / 3;
 
     gameBoardData[row][col] = player;
-    player *= -1; // change player
+    player *= -1; // switch player
+    player == 1 ? cell.classList.add("cross") : cell.classList.add("circle"); // placeMarker
+  };
 
-    player == 1 ? cell.classList.add("cross") : cell.classList.add("circle");
+  // check if win or draw
+  const checkData = () => {
+    //check for rows/columns
+    for (let i = 0; i < 3; i++) {
+      let row = gameBoardData[i][0] + gameBoardData[i][1] + gameBoardData[i][2];
+      let col = gameBoardData[0][i] + gameBoardData[1][i] + gameBoardData[2][i];
+      if (row == 3 || col == 3) {
+        console.log("player 1 wins");
+        displayController.reset();
+      } else if (row == -3 || col == -3) {
+        console.log("player 2 wins");
+        displayController.reset();
+      }
+    }
+    //check for diagonals
+    let diagonal1 =
+      gameBoardData[0][0] + gameBoardData[1][1] + gameBoardData[2][2];
+    let diagonal2 =
+      gameBoardData[0][2] + gameBoardData[1][1] + gameBoardData[2][0];
+    if (diagonal1 == 3 || diagonal2 == 3) {
+      console.log("player 1 wins");
+      displayController.reset();
+    } else if (diagonal1 == -3 || diagonal2 == -3) {
+      console.log("player 2 wins");
+      displayController.reset();
+    }
 
-    console.log(gameBoardData);
+    // check for tie
+    if (
+      gameBoardData[0].indexOf(0) == -1 &&
+      gameBoardData[1].indexOf(0) == -1 &&
+      gameBoardData[2].indexOf(0) == -1
+    ) {
+      // FIXME: win and tie happening at the last cell
+      console.log("tie");
+      displayController.reset();
+    }
   };
 
   return { gameBoardData };
 })();
 
-const displayController = (function (gameBoardData) {
-  // check if win or draw
-  const checkData = (gameBoardData) => {
-    console.log("check the data");
-
-    // show winning combinations
-    // after each turn, always check against these
-    // if there's a win or draw, call reset
-  };
-
+const displayController = (function () {
   const reset = () => {
-    // do something
     console.log("reset the game");
+    const cells = document.querySelectorAll("[data-cell]");
+    cells.forEach((cell) => {
+      cell.classList.remove("circle");
+      cell.classList.remove("cross");
+      // FIXME: remove once prop
+      cell.removeEventListener("click");
+    });
+    gameBoard.gameBoardData = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
   };
 
-  return { reset, checkData };
+  document.querySelector(".reset").addEventListener("click", reset);
+
+  return { reset };
 })();
 
 const Player = (playerNumber, playerMarker) => {
